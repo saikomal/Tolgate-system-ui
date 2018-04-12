52b3a9726c7c345203ee6ccfaf1929edd90fe33d
saikomal <saikomalpendela@yahoo.com>
2018-04-12 12:33:21 +0530

    added_control

--- index32.php
+++ index32.php
@@ -5,23 +5,15 @@ $username = "root";
 $password = "";
 $dbname = "Tolgate";
 $conn = new mysqli($servername, $username, $password, $dbname);
-$sql  = "select * from rfid where id = (select max(id) from rfid)";
+$sql  = "select * from control where id = (select max(id) from control)";
 
 $result = $conn->query($sql);
 if($result->num_rows>0){
     while($row = $result->fetch_assoc()){
        // echo "rfid";
        $id =  $row['id']; 
-       $rfid =  $row['rfid'];
-       $sql = "select * from authorised where value = '".$rfid."'";
-            $result1 = $conn->query($sql);
-            if($result1->num_rows>0){
-                        $rfid = "Authorised User";
-                        }
-                        else{
-                            $rfid = "Try again";
-                        }
-
+       $rfid =  $row['state'];
+       
             echo "id,";
             echo $id;            
             echo ",rfid,";
